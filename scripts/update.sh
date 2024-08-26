7d5537ffdfef9180d56ce767e526d68ef2c32f8c
Allen Porter <allen.porter@gmail.com>
2024-08-25 19:05:36 -0700

    Updates to esphome projects (#105)
    * Checkpoint current eink display
    
    * Add m5stack voice box
    
    * Upgrade esphome projects to newer version
    
    * Revert back to new esphome version
    
    * Update secret domain
    
    * Update domains

---
+++ scripts/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+esphome compile displays/eink-frame.yaml
+esphome upload displays/eink-frame.yaml
+
+esphome compile displays/frekvens.yaml
+esphome upload displays/frekvens.yaml
+
+esphome compile sensors/driveway-gate.yaml
+esphome upload sensors/driveway-gate.yaml
+
+esphome compile sensors/water-softener.yaml
+esphome upload sensors/water-softener.yaml
